de1ff394dfad46b48b01d5b00db62cadccf09a6d
kss682 <kss682@gmail.com>
2020-10-20 18:41:57 +0530

    models created

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from api.models import db
+
 
 
 def create_app():
     app = Flask(__name__)
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/postgres'
     # app.config.from_object('config.Config')
+    db.init_app(app)
 
     with app.app_context():
         from . import routes
+        db.create_all()
         return app
--- api/models.py
+++ api/models.py
@@ -1,4 +1,6 @@
-from . import db
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 class Product(db.Model):
