8aa902188803023b019003a644010cdcbe5e8ce2
kss682 <kss682@gmail.com>
2020-10-20 18:25:58 +0530

    models

---
+++ api/models.py
@@ -0,0 +1,21 @@
+from . import db
+
+
+class Product(db.Model):
+    __tablename__ = 'product'
+    id = db.Column(db.Integer, primary_key=True)
+    product = db.Column(db.String(255), nullable=False)
+
+
+class Location(db.Model):
+    __tablename__ = 'location'
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String(255), nullable=False)
+
+
+# class ProductMovement(db.Model):
+#     pass
+
+
+# class Inventory(db.Model):
+#     pass
