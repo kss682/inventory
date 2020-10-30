6b75ce6d8a83a6dcc1bbc77fb219406d1753a191
kss682 <kss682@gmail.com>
2020-10-30 19:33:58 +0530

    get routes added

--- api/routes.py
+++ api/routes.py
@@ -1,22 +1,33 @@
 from flask import Flask, jsonify, current_app as app
+from api.models import db, Product, Warehouse, ProductMovement, Inventory
 
-products = [
-    {
-        "id": 1,
-        "name": "Book"
-    },
-    {
-        "id": 2,
-        "name": "lap"
-    }
-]
+
+def serialization(query_results):
+    results = []
+    for i in query_results:
+        results.append(i.serialize())
+    return results
+
 
 @app.route('/products', methods=['GET'])
 def get_products():
+    products = serialization(Product.query.all())
     return jsonify(products)
 
 
-# @app.route('/products', method=['POST'])
-# def put_products():
-    
+@app.route('/locations', methods=['GET'])
+def get_locations():
+    locations = serialization(Location.query.all())
+    return jsonify(locations)
 
+
+@app.route('/movements', methods=['GET'])
+def get_movements():
+    movements = serialization(ProductMovement.query.all())
+    return jsonify(movements)
+
+
+@app.route('/inventory', methods=['GET'])
+def get_inventory():
+    inventory = serialization(Inventory.query.all())
+    return jsonify(inventory)
