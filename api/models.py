ad5ca4f97bbdae6e32aa7c0e895ca2ba5428628a
kss682 <kss682@gmail.com>
2020-10-30 22:07:17 +0530

    serializers added

--- api/models.py
+++ api/models.py
@@ -8,17 +8,11 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False, unique=True)
 
-    def serialize(self):
-        return {"id": self.id, "product": self.product}
-
 
 class Warehouse(db.Model):
     __tablename__ = 'warehouse'
     id = db.Column(db.Integer, primary_key=True)
     location = db.Column(db.String(255), nullable=False, unique=True)
-
-    def serialize(self):
-        return {"id": self.id, "location": self.location}
 
 
 class ProductMovement(db.Model):
@@ -29,26 +23,9 @@ class ProductMovement(db.Model):
     to_location = db.Column(db.String(255), db.ForeignKey('warehouse.location'))
     quantity = db.Column(db.Integer, nullable=False)
 
-    def serialize(self):
-        return {
-            "movement_id": self.movement_id,
-            "product_id": self.product_id,
-            "from_location": self.from_location,
-            "to_location": self.to_location,
-            "quantity": self.quantity
-        }
-
 
 class Inventory(db.Model):
     __tablename__ = 'inventory'
     prod_name = db.Column(db.String(255), db.ForeignKey('product.name'), primary_key=True)
     location = db.Column(db.String(255), db.ForeignKey('warehouse.location'), primary_key=True)
     quantity = db.Column(db.Integer, nullable=False)
-
-    def serialize(self):
-        return {
-            "prod_name": self.prod_name,
-            "location": self.location,
-            "quantity": self.quantity
-        }
-    
