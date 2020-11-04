7b4d0191a192531c766dcf6bbf491d26995c057a
kss682 <kss682@gmail.com>
2020-11-04 13:57:47 +0530

    POST method added

--- api/routes.py
+++ api/routes.py
@@ -17,12 +17,13 @@ class ProductApi(Resource):
         return jsonify(product_schemas.dump(products))
 
     def post(self):
-        product_data = self.reqparser.parse_args()        
-        product = Product(
+        product_data = self.reqparser.parse_args() 
+        product = Product(       
             name=product_data['name']
         )
         db.session.add(product)
         db.session.commit()
+        return jsonify({"status_code": 201})
 
 
 class WarehouseApi(Resource):
@@ -43,20 +44,59 @@ class WarehouseApi(Resource):
         )
         db.session.add(warehouse)
         db.session.commit() 
+        return jsonify({"status_code": 201})
 
 
 class ProductMovementApi(Resource):
+
+    def __init__(self):
+        self.reqparser = reqparse.RequestParser()
+        self.reqparser.add_argument('product_name', type=str, required=True)
+        self.reqparser.add_argument('from_location', type=str, required=True)
+        self.reqparser.add_argument('to_location', type=str, required=True)
+        self.reqparser.add_argument('quantity', type=int, required=True)
+
     def get(self):
         movements = ProductMovement.query.all()
         movements_schema = ProductMovementSchema(many=True)
         return jsonify(movements_schema.dump(movements))
 
+    def post(self):    
+        productmovement_data = self.reqparser.parse_args()
+        productmovement = ProductMovement(
+            prod_name=productmovement_data['prod_name'],
+            from_location=productmovement_data['from_location'],
+            to_location=productmovement_data['to_location'],
+            quantity=productmovement_data['quantity']
+        )
+        db.session.add(productmovement)
+        db.session.commit()
+        return jsonify({"status_code": 201})
+
 
 class InventoryApi(Resource):
+
+    def __init__(self):
+        self.reqparser = reqparse.RequestParser()
+        self.reqparser.add_argument('product_name', type=str, required=True)
+        self.reqparser.add_argument('location', type=str, required=True)
+        self.reqparser.add_argument('quantity', type=int, required=True)
+        
     def get(self):
         inventory = Inventory.query.all()
         inventory_schema = InventorySchema(many=True)
         return jsonify(inventory_schema.dump(inventory))
+
+    def post(self):
+        inventory_data = self.reqparser.parse_args()
+        inventory = Inventory(
+            prod_name=inventory_data['prod_name'],
+            location=inventory_data['location'],
+            quantity=inventory_data['quantity']
+        )
+        db.session.add(inventory_data)
+        db.session.commit()
+        return jsonify({"status_code": 200})
 
 
 api.add_resource(ProductApi, '/products', endpoint='products')
